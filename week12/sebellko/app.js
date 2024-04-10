bf649ff2a4606c41373240e03f955a5ca31e8fdc
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-04-10 17:28:02 +0900

    feat: 색상 변경, 지우개 기능 구현

--- week12/sebellko/app.js
+++ week12/sebellko/app.js
@@ -4,10 +4,28 @@ const INITIAL_SECOND = 59;
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll('#colorPallete li');
+const pen = document.getElementById('pen');
+const eraser = document.getElementById('eraser');
+
+pen.addEventListener('click', () => {
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 3;
+});
+eraser.addEventListener('click', () => {
+  ctx.strokeStyle = '#fff';
+  ctx.lineWidth = 15;
+});
+
+colors.forEach((color) => {
+  color.addEventListener('click', (event) => {
+    ctx.strokeStyle = event.target.classList.value;
+  });
+});
 
 canvas.width = 550;
 canvas.height = 700;
-ctx.lineWidth = '5';
+ctx.lineWidth = 3;
 ctx.strokeStyle = '#000';
 
 let isPainting = false;
@@ -18,6 +36,7 @@ canvas.addEventListener('mousedown', () => {
 
 canvas.addEventListener('mouseup', () => {
   isPainting = false;
+  ctx.beginPath();
 });
 
 canvas.addEventListener('mousemove', (event) => {
@@ -32,6 +51,9 @@ canvas.addEventListener('mousemove', (event) => {
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 const imgArr = document.querySelectorAll('#playerList li img');
+const answerInput = document.getElementById('answerInput');
+const submitButton = document.getElementById('submitButton');
+const answer = document.getElementById('answer');
 
 const captureCanvas = async (target) => {
   const { timeOut } = await setTimer(minute, second, INITIAL_SECOND);
@@ -46,3 +68,14 @@ const captureCanvas = async (target) => {
 for (const imageTag of imgArr) {
   await captureCanvas(imageTag);
 }
+
+submitButton.addEventListener('click', () => {
+  const playerAnswer = answerInput.value;
+  answer.innerText = playerAnswer;
+});
+
+answerInput.removeAttribute('disabled');
+
+const { timeOut } = await setTimer(minute, second, INITIAL_SECOND);
+
+if (timeOut) prompt('게임이 종료됐습니다.');
